3fbd4ce31a40ef417aebed5992f622b7bac50904
donawi <don.ashebir@gmail.com>
2021-01-01 20:38:55 +0300

    improved images and scripts

--- scripts/main.js
+++ scripts/main.js
@@ -1,3 +1,13 @@
-function hoverOff(img) {
-    img.setAttribute("src", "images/google-menu.png")
+function hoverOnUserDP(img) {
+    img.setAttribute("src", "images/google-menu-hover.png")
 }
+function hoverOffUserDP(img) {
+    img.setAttribute("src", "images/google-menu.png")
+    
+}
+// function hoverOnSearchBttn(bttn) {
+//     bttn.setAttribute("border", "2px solid #eee;")
+// }
+// function hoverOffSearchBttn(bttn) {
+//     bttn.setAttribute("border", "0px solid black")
+// }
