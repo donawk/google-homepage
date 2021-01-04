0e702fde7de212c299fd56235b5869c565a5ba65
donawi <don.ashebir@gmail.com>
2021-01-04 08:19:15 +0300

    finished?

--- scripts/main.js
+++ scripts/main.js
@@ -3,11 +3,4 @@ function hoverOnUserDP(img) {
 }
 function hoverOffUserDP(img) {
     img.setAttribute("src", "images/menu-small.png")
-    
 }
-// function hoverOnSearchBttn(bttn) {
-//     bttn.setAttribute("border", "2px solid #eee;")
-// }
-// function hoverOffSearchBttn(bttn) {
-//     bttn.setAttribute("border", "0px solid black")
-// }
