a8dead947286acb6c83b228c0feb3e6d80eb1261
donawi <don.ashebir@gmail.com>
2021-01-05 18:09:22 +0300

    finished transforming search bar, created a results page

--- scripts/main.js
+++ scripts/main.js
@@ -1,6 +1,26 @@
 function hoverOnUserDP(img) {
     img.setAttribute("src", "images/menu-small-hover.png")
 }
+
 function hoverOffUserDP(img) {
     img.setAttribute("src", "images/menu-small.png")
+}
+
+let searchBar = document.getElementById("search-bar")
+let activeSearch = document.getElementById("active-search")
+let isActive = false;
+let eraseSearch = document.getElementById("erase-search")
+let searchInput = document.getElementById("search-input")
+
+searchBar.onclick = function() {
+    searchInput.focus()
+}
+
+searchInput.oninput = function() {
+    activeSearch.style.display = "flex"
+}
+
+eraseSearch.onclick = function() {
+    activeSearch.style.display = "none"
+    searchInput.value = ""
 }
